4ea5984c868252989a22a710eb4cc2e74207337c
Janaina Borcate <borcatejana@gmail.com>
2022-02-14 19:53:42 -0300

    Utizando arquivo de dados

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 beforeEach(() => {
@@ -10,8 +12,23 @@ beforeEach(() => {
 });
 
     it('Deve fazer cadastro de faturamento', () => {
-        
+        EnderecoPage.editarEnderecoFaturamento('Andreia', 'Caron', 'Guide You Tech', 'Brasil', 'Rua das Flores', '01', 'Curitiba', 'Paraná', '83430-000', '41 9555-5555', 'andreiatest@teste.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
 
-        // cadastro de endereço
+    it.only('Deve fazer cadastro de faturamento - Utizando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
